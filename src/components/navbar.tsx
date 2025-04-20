e03f191be1af5bf2a3871d1110aede58717cb672
uday chauhan <chauhanuday842@gmail.com>
2025-04-20 23:32:15 +0530

    unused variable removed

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,5 @@
 
-import {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
